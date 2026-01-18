160aff34b864ada1ab48edec68b0d683062d22fa
Sara Caetano <misscryptosarcasm@gmail.com>
2026-01-18 20:16:54 +0000

    Configure Expo to use custom manifest.webmanifest instead of auto-generating

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,10 @@ const { getDefaultConfig } = require('expo/metro-config');
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
+// Ensure public files are copied to output
+config.resolver = {
+  ...config.resolver,
+  assetExts: [...(config.resolver?.assetExts || []), 'webmanifest'],
+};
+
 module.exports = config;
